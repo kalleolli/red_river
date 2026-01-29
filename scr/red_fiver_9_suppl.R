4cd0e47f5ead7fbb68cbd80c25e966359867191a
kalleolli <103435140+kalleolli@users.noreply.github.com>
2026-01-29 14:05:58 +0200

    Delete scr directory

--- scr/red_fiver_9_suppl.R
+++
@@ -1,6 +0,0 @@
-# supplementary table 1
-# Detailed numerical summary of the permutational feature importances of all 33 features, split by model type (rf vs gb) and target variable (all red algae or Batrachospermum spp) is given in the Supplementary table 1
-
-# The relationship with nitrate was non-linear – positive at the most data rich region of < 0.8 mg L-1, but dropped abruptly thereafter (Fig. S1)
-# The depth of the river channel, flow velocity, availability of suitable substrate and shading conditions are important for the occurrence of the species. 
-# Red algae have a wide range of flow velocity tolerance, but the average values are relatively narrow, 0.3-0.6 m s-1 
--- scr/red_river_one.R
+++
@@ -1,5 +0,0 @@
-
-
-
-system("R CMD BATCH ./scr/red_river_6_figs1.R red_riv_out.txt")
-system("R CMD BATCH ./scr/ToRedRiverGit_v2.R ToRedRiverGit.txt")
